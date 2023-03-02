ed4b14f65c3114df2148450ec80e825b5e636df2
prego <pedropregueiro@gmail.com>
2023-03-02 18:34:44 +0100

    day 1, part 2

--- 01/main.py
+++ 01/main.py
@@ -1,22 +1,22 @@
 if __name__ == "__main__":
+    elves = []
+
     with open("input.txt") as f:
         lines = f.readlines()
 
-    curr_elf_index = 1
     curr_elf_calories_intake = 0
-
-    heaviest_elf_index = -1
-    heaviest_elf_intake = -1
-
     for line in lines:
         if line == "\n":
-            if curr_elf_calories_intake > heaviest_elf_intake:
-                heaviest_elf_intake = curr_elf_calories_intake
-                heaviest_elf_index = curr_elf_index
-
-            curr_elf_index += 1
+            elves.append(curr_elf_calories_intake)
             curr_elf_calories_intake = 0
         else:
             curr_elf_calories_intake += int(line)
 
-    print(f"The heaviest elf is {heaviest_elf_index} with {heaviest_elf_intake} calories")
+    elves.append(curr_elf_calories_intake)
+    curr_elf_calories_intake = 0
+
+    sorted_elves = sorted(elves, reverse=True)
+    elf_index = elves.index(sorted_elves[0]) + 1
+
+    print(f"[part 1] The heaviest elf is {elf_index} with {sorted_elves[0]} calories")
+    print(f"[part 2] The 3 heaviest elves are carrying {sum(sorted_elves[:3])} calories")
