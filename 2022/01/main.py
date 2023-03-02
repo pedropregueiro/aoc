bd62955f125d50910cb538993287428dfc4e787d
prego <pedropregueiro@gmail.com>
2023-03-02 18:46:26 +0100

    no need for extra reset + friendly comment

--- 2022/01/main.py
+++ 2022/01/main.py
@@ -13,10 +13,9 @@ if __name__ == "__main__":
             curr_elf_calories_intake += int(line)
 
     elves.append(curr_elf_calories_intake)
-    curr_elf_calories_intake = 0
 
     sorted_elves = sorted(elves, reverse=True)
-    elf_index = elves.index(sorted_elves[0]) + 1
+    elf_index = elves.index(sorted_elves[0]) + 1  # elves is 0-indexed
 
     print(f"[part 1] The heaviest elf is {elf_index} with {sorted_elves[0]} calories")
     print(f"[part 2] The 3 heaviest elves are carrying {sum(sorted_elves[:3])} calories")
