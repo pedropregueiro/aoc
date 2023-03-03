410cabff838fa2065135b35a89bb66a5501ac5f9
prego <pedropregueiro@gmail.com>
2023-03-03 15:36:46 +0100

    day 3, part 2

--- 2022/03/main.py
+++ 2022/03/main.py
@@ -1,25 +1,40 @@
 from string import ascii_letters
+from typing import List, Set
 
 
-def find_repeated_element(comp1: str, comp2: str) -> str:
+def batch_list(list_: List, group_size: int):
+    total_length = len(list_)
+    for ndx in range(0, total_length, group_size):
+        yield list_[ndx : min(ndx + group_size, total_length)]
+
+
+def find_repeated_elements(comp1: str, comp2: str) -> Set[str]:
     repeated_elems = list(map(lambda elem: elem in comp2, comp1))
-    return comp1[repeated_elems.index(True)]
+    return {comp1[index] for index, elem in enumerate(repeated_elems) if elem is True}
+
+
+def find_group_repeated_element(group: List[str]) -> str:
+    # this could probably be done in a smarter way
+    repeated = set(group[0])
+    for rem_rucksack in group[1:]:
+        repeated = find_repeated_elements(str(repeated), rem_rucksack)
+
+    if len(repeated) != 1:
+        raise Exception("expected only 1 repeated element")
+
+    return list(repeated)[0]
 
 
 def calc_sum_priorities(filename) -> int:
     with open(filename) as f:
-        lines = f.readlines()
+        lines = f.read().splitlines()
 
     sum_priorities = 0
 
-    for line in lines:
-        line = line.strip()
-        split_index = int(len(line) / 2)
-        comp1, comp2 = line[:split_index], line[-split_index:]
-
-        repeated = find_repeated_element(comp1, comp2)
+    for group_lines in batch_list(lines, group_size=3):
+        repeated = find_group_repeated_element(group_lines)
         alphabet_index = ascii_letters.index(repeated) + 1
-        print(f"{comp1} | {comp2} | repeated item: {repeated} | {alphabet_index}")
+        print(f"{group_lines} | repeated item: {repeated} | {alphabet_index}")
 
         sum_priorities += alphabet_index
 
@@ -28,5 +43,5 @@ def calc_sum_priorities(filename) -> int:
 
 
 if __name__ == "__main__":
-    assert calc_sum_priorities("test_input.txt") == 157
+    assert calc_sum_priorities("test_input.txt") == 70
     calc_sum_priorities("input.txt")
