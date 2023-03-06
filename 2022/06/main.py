20d95612f330f7d08ed7b33dfded8cdb8d6a00fc
prego <pedropregueiro@gmail.com>
2023-03-06 23:00:46 +0100

    day 6, part 2

--- 2022/06/main.py
+++ 2022/06/main.py
@@ -1,9 +1,9 @@
-def get_start_of_packet(signal: str) -> int:
+def get_start_of_general(signal: str, distinct_count: int) -> int:
     buff_chars = []
     for marker, char in enumerate(signal, start=1):
         buff_chars.append(char)
-        last4 = buff_chars[-4:]
-        if len(set(last4)) == 4:
+        last4 = buff_chars[-distinct_count:]
+        if len(set(last4)) == distinct_count:
             return marker
 
 
@@ -11,13 +11,30 @@ def get_start_of_packet_from_file(filename: str):
     with open(filename) as f:
         signal = f.read().strip()
 
-    start_of_packet = get_start_of_packet(signal)
+    start_of_packet = get_start_of_general(signal, distinct_count=4)
     print(f"Start of packet is {start_of_packet}")
 
 
+def get_start_of_message_from_file(filename: str):
+    with open(filename) as f:
+        signal = f.read().strip()
+
+    start_of_message = get_start_of_general(signal, distinct_count=14)
+    print(f"Start of message is {start_of_message}")
+
+
 if __name__ == "__main__":
-    assert get_start_of_packet("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 7
-    assert get_start_of_packet("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5
-    assert get_start_of_packet("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10
-    assert get_start_of_packet("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11
+    # part 1
+    assert get_start_of_general("mjqjpqmgbljsphdztnvjfqwrcgsmlb", distinct_count=4) == 7
+    assert get_start_of_general("bvwbjplbgvbhsrlpgdmjqwftvncz", distinct_count=4) == 5
+    assert get_start_of_general("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", distinct_count=4) == 10
+    assert get_start_of_general("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", distinct_count=4) == 11
     get_start_of_packet_from_file("input.txt")
+
+    # part 2
+    assert get_start_of_general("mjqjpqmgbljsphdztnvjfqwrcgsmlb", distinct_count=14) == 19
+    assert get_start_of_general("bvwbjplbgvbhsrlpgdmjqwftvncz", distinct_count=14) == 23
+    assert get_start_of_general("nppdvjthqldpwncqszvftbrmjlhg", distinct_count=14) == 23
+    assert get_start_of_general("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", distinct_count=14) == 29
+    assert get_start_of_general("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", distinct_count=14) == 26
+    get_start_of_message_from_file("input.txt")
